289236a43e4e24037e7619c6113f64a10a5bb337
aquilax <aquilax@gmail.com>
2018-07-22 13:44:14 +0200

    Fixed tests

--- test/sparkline_console_test.dart
+++ test/sparkline_console_test.dart
@@ -4,17 +4,17 @@ import 'package:test/test.dart';
 void main() {
   group('A group of tests', () {
     test('generates spark line', () {
-      var points = [1, 2, 3, 4, 5, 6, 7, 8];
+      List<num> points = [1, 2, 3, 4, 5, 6, 7, 8];
       expect(new SparkLine().generate(points), equals('▁▂▃▄▅▆▇█'));
     });
 
     test('generates custom spark line', () {
-      var points = [1, 2, 3, 4, 5, 6, 7, 8];
+      List<num> points = [1, 2, 3, 4, 5, 6, 7, 8];
       expect(new SparkLine('0123456789').generate(points), equals('01245789'));
     });
 
     test('empty list generates empty response', () {
-      var points = [];
+      List<num> points = [];
       expect(new SparkLine().generate(points), equals(''));
     });
   });
